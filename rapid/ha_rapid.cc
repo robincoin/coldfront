00366670ce2d056fce3eac9ad5515f1ef89decae
Justin Swanhart <greenlion@gmail.com>
2025-11-20 03:50:34 -0500

    Update license comment and copywrite

--- rapid/ha_rapid.cc
+++ rapid/ha_rapid.cc
@@ -1,11 +1,11 @@
-/* Copyright (c) 2018, 2025, Oracle and/or its affiliates.
+/* Copyright (c) 2025, Justin Swanhart
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License, version 2.0,
+   it under the terms of the GNU General Public License, version 3.0,
    as published by the Free Software Foundation.
 
    This program is designed to work with certain software (including
-   but not limited to OpenSSL) that is licensed under separate terms,
+   but not limited to DuckDB) that is licensed under separate terms,
    as designated in a particular file or component or in included license
    documentation.  The authors of MySQL hereby grant you an additional
    permission to link the program and your derivative works with the
